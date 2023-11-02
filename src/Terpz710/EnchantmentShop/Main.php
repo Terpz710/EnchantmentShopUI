64e67ddfd8cd3caf3964f26960890fe5a7a26d85
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-02 05:40:52 -0700

    Update Main.php

--- src/Terpz710/EnchantmentShop/Main.php
+++ src/Terpz710/EnchantmentShop/Main.php
@@ -13,12 +13,6 @@ class Main extends PluginBase {
     public function onEnable(): void {
         $libEco = new libEco();
 
-        if ($libEco->isInstall()) {
-            $this->getLogger()->info("libEco is installed and working.");
-        } else {
-            $this->getLogger()->error("libEco is not properly installed or configured.");
-            return;
-        }
         $this->saveDefaultConfig();
         $this->saveResource("Shop.yml");
         $this->getServer()->getCommandMap()->register("eshop", new EShopCommand($this, $libEco));
