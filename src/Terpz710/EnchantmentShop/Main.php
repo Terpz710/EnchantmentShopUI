728e84fc31c00dfd2aec5d5885db1697c99514d1
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-02 06:02:27 -0700

    Update Main.php

--- src/Terpz710/EnchantmentShop/Main.php
+++ src/Terpz710/EnchantmentShop/Main.php
@@ -6,15 +6,12 @@ namespace Terpz710\EnchantmentShop;
 
 use pocketmine\plugin\PluginBase;
 use Terpz710\EnchantmentShop\Command\EShopCommand;
-use davidglitch04\libEco\libEco;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
-        $libEco = new libEco();
-
         $this->saveDefaultConfig();
         $this->saveResource("Shop.yml");
-        $this->getServer()->getCommandMap()->register("eshop", new EShopCommand($this, $libEco));
+        $this->getServer()->getCommandMap()->register("eshop", new EShopCommand($this));
     }
 }
